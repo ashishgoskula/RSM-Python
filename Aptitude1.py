069a3761a083c1307092cd0a1ac5f0ade460211c
ashishgoskula <83582401+ashishgoskula@users.noreply.github.com>
2022-01-24 19:44:42 +0530

    Create Aptitude1.py

---
+++ Aptitude1.py
@@ -0,0 +1,6 @@
+speed = int(input('Speed of train: '))
+speed = speed * (5 / 18)
+print('Speed of train is: ', speed)
+time = int(input('Time taken by train: '))
+length = speed * time
+print('Length of train: ', length)
