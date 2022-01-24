88f410fd5770187319652ace94a6e0d98cff4868
ashishgoskula <83582401+ashishgoskula@users.noreply.github.com>
2022-01-24 20:49:49 +0530

    Create Aptitude5.py

---
+++ Aptitude5.py
@@ -0,0 +1,8 @@
+length = int(input('Enter the train length: '))
+speed = int(input('Enter speed of train: '))
+man_speed = int(input('Enter speed of man: '))
+x = (speed - manspeed)
+print('Relative speed in kmph is: ', x)
+x = (x * (5 / 18))
+time = length / x
+print('Time taken by the  train: 1', time)
