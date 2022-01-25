2b38c641034102d759a33d167288ae3b0330003a
ashishgoskula <83582401+ashishgoskula@users.noreply.github.com>
2022-01-25 15:03:54 +0530

    Update Aptitude4.py

--- Aptitude4.py
+++ Aptitude4.py
@@ -3,7 +3,7 @@ speed = speed * (5 / 18)
 print("Train speed: ", speed)
 length = int(input('Enter length of train: '))
 platform = int(input('Enter length of platform: '))
-distance = lemgyh + platform
+distance = length + platform
 print("Total distance is: ", distance)
 time = (distance / speed)
 print("Time taken to cross the platform: ", time)
