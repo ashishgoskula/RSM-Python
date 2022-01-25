28abe5952b36649a11d28f75316dd4328e9eba66
ashishgoskula <83582401+ashishgoskula@users.noreply.github.com>
2022-01-25 15:04:41 +0530

    Update Aptitude2.py

--- Aptitude2.py
+++ Aptitude2.py
@@ -2,5 +2,5 @@ length = int(input("Enter Train Length: "))
 speed = int(input("Enter Man's Speed: "))
 time = int(input("Enter time taken: "))
 relative_speed = length / time * (18 / 5)
-train_speed = relativespeed + speed
-print("The train speed is:", trainspeed)
+train_speed = relative_speed + speed
+print("The train speed is:", train_speed)
